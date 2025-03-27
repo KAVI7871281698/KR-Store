a11404b0838ac9e6592ef0ada8c8cb38e2fe6830
KAVI7871281698 <kavipriyan292004@gmail.com>
2025-03-27 16:09:14 +0530

    delete the extra code

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -36,25 +36,6 @@ class add_to_cart(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  
     order_date = models.DateTimeField(auto_now_add=True)  # Order date
 
-# class Ordernow(models.Model): 
-#     name=models.CharField(max_length=20, null=True)
-#     email = models.CharField(max_length=50, null=True)
-#     quantity = models.IntegerField(default=1)
-#     product_img = models.ImageField(upload_to='uploads/',null=True)
-#     product_name = models.CharField(max_length=50,null=True)
-#     product_price = models.DecimalField(max_digits=10, decimal_places=2)
-#     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
-#     order = models.ForeignKey(add_to_cart, on_delete=models.CASCADE)  
-#     order_date = models.DateTimeField(auto_now_add=True) 
-#     address = models.TextField(null=True, blank=True)
-#     status = models.CharField(
-#         max_length=50,
-#         choices=[('Pending', 'Pending'), ('Processing', 'Processing'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Cancelled', 'Cancelled')],
-#         default='Pending'
-#     )
-#     order_month = models.CharField(max_length=10, default=datetime.datetime.now().strftime('%B'), editable=False)  
-#     order_year = models.IntegerField(default=datetime.datetime.now().year, editable=False)  
-
 class Ordernow(models.Model):
     name = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=50, null=True)
